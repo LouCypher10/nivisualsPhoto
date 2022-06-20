5a61adeb2ac99c1f945ff88983d332a68a867eda
Lou Cypher <98168195+LouCypher10@users.noreply.github.com>
2022-06-20 09:29:08 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  width: 100%;
+  height: 150vh;
+  background-image: url("./assets/img/together.jpg");
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
 }
 </style>
